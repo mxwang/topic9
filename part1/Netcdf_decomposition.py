7a4c72004697ca17aa23a1ad619e4d6e01532b2a
mwang <muxuan311@gmail.com>
2015-04-10 14:24:38 -0500

    bug fix

--- part1/Netcdf_decomposition.py
+++ part1/Netcdf_decomposition.py
@@ -417,7 +417,7 @@ class Netcdf_Reader:
             #print xdim, ydim, zdim
 
             data_size = xdim * ydim * zdim
-            xy_size = xdim * ydim
+            
             #x, y, z , x+dimX*(y+dimY*z)
             data = unpack('25000000f', raw_data[12:16 * data_size])
             data_array = np.asarray(data)
@@ -474,8 +474,8 @@ class Netcdf_Reader:
         print "Subvolume <", bound[0], bound[3], "> <" ,bound[1], bound[4], "> <", bound[2], bound[5],"> is assigned to process <", str(rank), ">"
 
         #broad cast to all other processes slice by slice along z
-        sliced_data = np.zeros(xy_size)
-        mean_data = []
+        sliced_data = np.zeros(250000)
+        
         for zidx in range(zdim):
             if(rank == 0):
                 #x + dimX * (y + dimY * z)
@@ -486,13 +486,14 @@ class Netcdf_Reader:
                 sliced_data = None
             
             sliced_data = comm.bcast(sliced_data,root = 0)
-            
+            val = 0
             if(zdim > bound[2] and zdim < bound[5]):
                 myidx_min = bound[0] + 500 * (bound[1] + 500 * zdim)
                 myidx_max = bound[3] + 500 * (bound[4] + 500 * zdim)
                 for i in xrange(myidx_min, myidx_max + 1):
-                    mean_data.append(sliced_data[i])
-        print "Process <", rank, "> has data < ",bound[0], bound[3], "> <" ,bound[1], bound[4], "> <", bound[2], bound[5], ", mean = "
+                    val += sliced_data[i]
+            mean = val/(myidx_max - myidx_min + 1)
+        print "Process <", rank, "> has data < ",bound[0], bound[3], "> <" ,bound[1], bound[4], "> <", bound[2], bound[5], ", mean = ", mean
 
             
     #def ckeck_bound(self, ):
