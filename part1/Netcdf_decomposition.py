da3ff1a2d9366275e4ebdeef803e9c167469c3c4
mwang <muxuan311@gmail.com>
2015-04-12 15:51:21 -0500

    debug

--- part1/Netcdf_decomposition.py
+++ part1/Netcdf_decomposition.py
@@ -483,7 +483,7 @@ class Netcdf_Reader:
         local_buffer = []
         
         for i in xrange(0, zdim):
-            print "hey zidx in for loop is:", zidx
+            print "hey zidx in for loop is:", i
             if(rank == 0):
                 
                 sliced_data = np.zeros(250000)
@@ -491,7 +491,7 @@ class Netcdf_Reader:
                 sidx_min = 0 + 500 * (0 + 500 * zidx)
                 sidx_max = 499 + 500 * (499 + 500 * zidx)
                 sliced_data = data_array[sidx_min:sidx_max + 1]
-                print "for zidx = ", zidx, "sliced_data:", sliced_data[:]
+                print "for zidx = ", i, "sliced_data: [sidx_min]", sidx_min, "[sidx_max]", sidx_max + 1
             else:
                 sliced_data = None
                 
