967edec9d3d88da6068aa53f5eb74f3a2d38ba24
mwang <muxuan311@gmail.com>
2015-04-17 13:01:35 -0500

    back to gather

--- part1/Netcdf_decomposition.py
+++ part1/Netcdf_decomposition.py
@@ -430,10 +430,12 @@ class Netcdf_Reader:
         zdim = 3
 
         data_size = xdim * ydim * zdim
+        data_array = np.asarray(27)
+        print data_array, data_array.size
 
         if(rank == 0):
-            data_array = np.asarray(27)
-            print data_array, data_array.size
+            # data_array = np.asarray(27)
+            # print data_array, data_array.size
 
         #------------------------
 
