fb38bf0111de34f5f621eece443594f5e5ad701c
mwang <muxuan311@gmail.com>
2015-04-16 20:28:32 -0500

    debugging

--- part1/Netcdf_decomposition.py
+++ part1/Netcdf_decomposition.py
@@ -431,7 +431,7 @@ class Netcdf_Reader:
             bound = np.zeros((size, 6), dtype = np.int)
            
             
-            for ridx in xrange(1, size):
+            for ridx in xrange(0, size-1):
                 
                 zidx = (ridx) % zpart
                 yidx = ((ridx) / zpart) % ypart
@@ -451,23 +451,23 @@ class Netcdf_Reader:
                     zmax = zdim -1
 
                 if(xmin == 0):
-                    bound[ridx][0] = xmin
-                    bound[ridx][3] = xmax
+                    bound[ridx+1][0] = xmin
+                    bound[ridx+1][3] = xmax
                 else:
-                    bound[ridx][0] = xmin + 1
-                    bound[ridx][3] = xmax
+                    bound[ridx+1][0] = xmin + 1
+                    bound[ridx+1][3] = xmax
                 if(ymin == 0):
-                    bound[ridx][1] = ymin
-                    bound[ridx][4] = ymax
+                    bound[ridx+1][1] = ymin
+                    bound[ridx+1][4] = ymax
                 else:
-                    bound[ridx][1] = ymin + 1
-                    bound[ridx][4] = ymax
+                    bound[ridx+1][1] = ymin + 1
+                    bound[ridx+1][4] = ymax
                 if(zmin == 0):
-                    bound[ridx][2] = zmin
-                    bound[ridx][5] = zmax
+                    bound[ridx+1][2] = zmin
+                    bound[ridx+1][5] = zmax
                 else:
-                    bound[ridx][2] = zmin + 1
-                    bound[ridx][5] = zmax
+                    bound[ridx+1][2] = zmin + 1
+                    bound[ridx+1][5] = zmax
 
                 print "rank", ridx, "boundary:", bound[ridx][:]
            
