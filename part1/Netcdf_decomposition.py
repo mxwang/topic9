4a9dfe336f8a8882efc8d6407c37c8847a984a9c
mwang <muxuan311@gmail.com>
2015-04-16 21:25:09 -0500

    debugging

--- part1/Netcdf_decomposition.py
+++ part1/Netcdf_decomposition.py
@@ -507,8 +507,8 @@ class Netcdf_Reader:
                       
             if(i >= bound[2] and i <= bound[5] and i != 0):
                 local_buffer = np.append(local_buffer, self.copy_local_data(i,bound,sliced_data,rank))
-        
-        print "Process <", rank , "> has data <", bound[0], bound[3], "> <" ,bound[1], bound[4], "> <", bound[2], bound[5],">,  mean = <",  np.mean(local_buffer), ">"
+        if(rank != 0):
+            print "Process <", rank , "> has data <", bound[0], bound[3], "> <" ,bound[1], bound[4], "> <", bound[2], bound[5],">,  mean = <",  np.mean(local_buffer), ">"
             
             
     def copy_local_data(self, z, bound, sliced_data, rank):
