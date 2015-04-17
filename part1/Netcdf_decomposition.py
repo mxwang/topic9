adf03ef0fb78c85cc4c6263496ab964da2cd475d
mwang <muxuan311@gmail.com>
2015-04-17 06:49:45 -0500

    gatering debug

--- part1/Netcdf_decomposition.py
+++ part1/Netcdf_decomposition.py
@@ -517,8 +517,9 @@ class Netcdf_Reader:
             mean_all = np.zeros(size)
             print "mean all before:", mean_all
         MPI.COMM_WORLD.Barrier()
-        print "rank", rank, "mean val:", mean_val
-        mean_all = MPI.COMM_WORLD.gather(np.mean(local_buffer),root = 0)
+        if(rank !== 0)
+            print "rank", rank, "mean val:", mean_val
+            mean_all = MPI.COMM_WORLD.gather(mean_val,root = 0)
         if(rank == 0):
             print "mean_all after", mean_all
             
